60909cefa0aa89a7a7bea36a12305d33454be187
ANUJ <nagar.anuj14@gmail.com>
2018-02-14 00:10:32 +0530

    sdas

---
+++ src/main/java/org/anuj/swagger/Swaggerdemo/configuration/Config.java
@@ -0,0 +1,29 @@
+package org.anuj.swagger.Swaggerdemo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Config {
+
+	@Bean
+	public Docket productapi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("org.anuj.swagger.Swaggerdemo.Restcontroller"))
+				.paths(PathSelectors.any()).build().apiInfo(metaData());
+
+	}
+	private ApiInfo metaData() {
+        ApiInfo apiinfo=new ApiInfo("SpringBootRestApi", "Spring Boot REST API for Online Store", "1.0", "Terms of service", "Anuj", "Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
+        return apiinfo;
+	}
+}
